bacd211079c039cfea6d96360c565b6209a26a58
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-06-22 19:18:30 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './App.css'
 
-function App() {
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/'>
+      <Route index element={<>Welcome</>}/>
 
-  return (
-    <>
-      Welcome
-    </>
+    </Route>
   )
-}
+)
+
+const App = () => (
+  <RouterProvider router={router}/>
+)
 
 export default App
